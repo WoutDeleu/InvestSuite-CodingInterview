f58992b56113acfe72fe2f26502c3b74e78c55b9
WoutDeleu <wout.deleu@gmail.com>
2023-11-09 10:15:33 +0100

    Refactoring folders and files

--- CodingTest/src/digit_counter/digit_counter_mathematical.py
+++ CodingTest/src/digit_counter/digit_counter_mathematical.py
@@ -1,24 +1,6 @@
 from CodingTest.src.util import verify_input, extract_positive_integer
 
 
-def digit_counter_conversion(number) -> int:
-    """
-    Count the number of digits in a positive integer. Based on conversion to a string, and the length of that string.
-
-    Args:
-        number: a numeric value / any Python type in the standard library that behaves as a number
-
-    Returns:
-        int: the number of digits needed to write the integer part of that number
-    """
-
-    # Handle and converts input to correct format
-    verify_input(number)
-    number: int = extract_positive_integer(number)
-
-    return len(str(number))
-
-
 def digit_counter_mathematical(number) -> int:
     """
     Count the number of digits in a positive integer. Based on mathematical operations.
--- CodingTest/tests/test_digit_counter_mathematical.py
+++ CodingTest/tests/test_digit_counter_mathematical.py
@@ -1,59 +1,39 @@
 from decimal import Decimal
 
-from CodingTest.src.digit_counter import digit_counter_conversion, digit_counter_mathematical
+from CodingTest.src.digit_counter.digit_counter_mathematical import digit_counter_mathematical
 
 
 def test_integers():
-    assert digit_counter_conversion(12345) == 5
-    assert digit_counter_conversion(100) == 3
-    assert digit_counter_conversion(999) == 3
-
     assert digit_counter_mathematical(12345) == 5
     assert digit_counter_mathematical(100) == 3
     assert digit_counter_mathematical(999) == 3
 
 
 def test_null():
-    assert digit_counter_conversion(0) == 1
     assert digit_counter_mathematical(0) == 1
 
 
 def test_negative():
-    assert digit_counter_conversion(-12345) == 5
-    assert digit_counter_conversion(-10) == 2
-    assert digit_counter_conversion(-999) == 3
-
     assert digit_counter_mathematical(-12345) == 5
     assert digit_counter_mathematical(-10) == 2
     assert digit_counter_mathematical(-999) == 3
 
 
 def test_floats():
-    assert digit_counter_conversion(1.2345) == 1
-    assert digit_counter_conversion(1.0) == 1
-    assert digit_counter_conversion(0.999) == 1
-
     assert digit_counter_mathematical(1.2345) == 1
     assert digit_counter_mathematical(1.0) == 1
     assert digit_counter_mathematical(0.999) == 1
 
-def test_complex():
-    assert digit_counter_conversion(1 + 2j) == 1
-    assert digit_counter_conversion(14.56 + 2j) == 2
 
+def test_complex():
     assert digit_counter_mathematical(1 + 2j) == 1
     assert digit_counter_mathematical(14.56 + 2j) == 2
 
-def test_fractions():
-    assert digit_counter_conversion(1 / 2) == 1
-    assert digit_counter_conversion(7776 / 7) == 4
 
+def test_fractions():
     assert digit_counter_mathematical(1 / 2) == 1
     assert digit_counter_mathematical(7776 / 7) == 4
 
 
 def test_decimal():
-    assert digit_counter_conversion(Decimal(211.2345)) == 3
-
     assert digit_counter_mathematical(Decimal(211.2345)) == 3
-
